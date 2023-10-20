7849bd112230d40290663122e6e9b7af7c93c47f
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-10-20 11:28:59 -0300

    Created Cart components and portals.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Cart />
+      <Header />
       <main>
         <Meals />
       </main>
